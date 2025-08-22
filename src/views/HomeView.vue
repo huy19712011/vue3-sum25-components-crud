56fd52f998e2a03f98f0a1da1470dbe03dfccd1f
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-08-22 22:36:37 +0700

    Init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="about">
+    <h1>This is the home page</h1>
+  </div>
+</template>
---
+++ src/views/StudentView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="about">
+    <h1>This is the student page</h1>
+  </div>
+</template>
