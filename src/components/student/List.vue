2795c8ef69ef16e2d8ae8ed340c2aec874a4659f
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-08-23 22:49:15 +0700

    List component

---
+++ src/components/student/List.vue
@@ -0,0 +1,44 @@
+<script setup>
+const props = defineProps({
+  students: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+<template>
+  <div class="card">
+    <div class="card-header">
+      <h2>Students</h2>
+    </div>
+    <div class="card-body">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Created At</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(student, index) in students" :key="student.id">
+            <td>{{ index + 1 }}</td>
+            <td>{{ student.id }}</td>
+            <td>{{ student.name }}</td>
+            <td>{{ student.email }}</td>
+            <td>{{ student.phone }}</td>
+            <td>{{ student.created_at }}</td>
+            <td>
+              <button class="btn btn-success" @click="editStudent(student)">Edit</button>
+              <button class="btn btn-danger" @click="deleteStudent(student.id)">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
--- src/views/StudentView.vue
+++ src/views/StudentView.vue
@@ -1,7 +1,37 @@
-<script setup></script>
+<script setup>
+import List from "@/components/student/List.vue";
+import { ref } from "vue";
+
+const students = ref([
+  {
+    id: 1,
+    name: "student 1",
+    email: "email 1",
+    phone: "phone 1",
+    created_at: "2024-10-17",
+    updated_at: "2024-10-18",
+  },
+  {
+    id: 2,
+    name: "student 2",
+    email: "email 2",
+    phone: "phone 2",
+    created_at: "2024-10-17",
+    updated_at: "2024-10-18",
+  },
+  {
+    id: 3,
+    name: "student 3",
+    email: "email 3",
+    phone: "phone 3",
+    created_at: "2024-10-17",
+    updated_at: "2024-10-18",
+  },
+]);
+</script>
 
 <template>
-  <div class="about">
-    <h1>This is the student page</h1>
+  <div class="container">
+    <List :students="students"></List>
   </div>
 </template>
