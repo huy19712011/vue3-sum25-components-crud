6f775da10f1ab2f83c3c8074a73396ed15436a13
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-08-24 11:06:31 +0700

    Update CSS

--- src/components/student/List.vue
+++ src/components/student/List.vue
@@ -17,38 +17,53 @@ const deleteStudent = (student) => {
 };
 </script>
 <template>
-  <div class="card">
-    <div class="card-header">
-      <h2>Students</h2>
-    </div>
-    <div class="card-body">
-      <table class="table table-bordered">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Created At</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(student, index) in students" :key="student.id">
-            <td>{{ index + 1 }}</td>
-            <td>{{ student.id }}</td>
-            <td>{{ student.name }}</td>
-            <td>{{ student.email }}</td>
-            <td>{{ student.phone }}</td>
-            <td>{{ student.created_at }}</td>
-            <td>
-              <button class="btn btn-success" @click="editStudent(student)">Edit</button>
-              <button class="btn btn-danger" @click="deleteStudent(student)">Delete</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+  <div class="container mt-5">
+    <div class="card">
+      <div class="card-header">
+        <h2>Students</h2>
+      </div>
+      <div class="card-body">
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Created At</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(student, index) in students"
+              :key="student.id"
+            >
+              <td>{{ index + 1 }}</td>
+              <td>{{ student.id }}</td>
+              <td>{{ student.name }}</td>
+              <td>{{ student.email }}</td>
+              <td>{{ student.phone }}</td>
+              <td>{{ student.created_at }}</td>
+              <td>
+                <button
+                  class="btn btn-success"
+                  @click="editStudent(student)"
+                >
+                  Edit
+                </button>
+                <button
+                  class="btn btn-danger"
+                  @click="deleteStudent(student)"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
